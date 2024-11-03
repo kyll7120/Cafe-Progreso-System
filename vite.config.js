2d2befe294c18e7795f6e95b9c8db520db2d9727
kyller7120 <riveramarlon113@gmail.com>
2024-11-03 06:22:42 -0600

    Cambioss

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,14 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',  // Archivo CSS de entrada
-                'resources/js/app.js',     // Archivo JS de entrada
+                'resources/css/app.css',               // Archivo CSS de entrada
+                'resources/js/app.js',                  // Archivo JS de entrada principal
+                'resources/views/administracion/scriptAdministracion.js', // Agrega tu archivo scriptAdministracion.js aquí
             ],
-            refresh: true,                // Habilita el refresh automático durante el desarrollo
+            refresh: true,                             // Habilita el refresh automático durante el desarrollo
         }),
     ],
     build: {
-        outDir: 'public/build',        // Directorio de salida para los archivos compilados
+        outDir: 'public/build',                     // Directorio de salida para los archivos compilados
     },
 });
